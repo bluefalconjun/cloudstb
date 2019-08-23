2c6a726dd7e3f6837bdc1e9a6dfa44c0a46fa53d
juxu <falcon.xu@montage-lz.com>
2019-08-23 17:57:58 +0800

    add init Make

---
+++ client/inc/common.h
@@ -0,0 +1,9 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_cmd(int argc, char** argv);
+
+#endif
--- client/src/main.c
+++ client/src/main.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "common.h"
+
 
 int main(int argc, char** argv) {
 
+    show_cmd(argc, argv);
 
     return 0;
+}
+
+void show_cmd(int argc, char** argv){
+    int i=0;
+
+    printf("cmd is:\n");
+
+    while(i<argc) {
+        printf("%s ", argv[i]);
+        i++;
+    }
+    printf("\n");
+
+    return;
 }
