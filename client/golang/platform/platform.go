0b38d3a085f6bae058feb79220638d9ce1e4c2fd
Xu Jun <jun.xu.falcon@gmail.com>
2019-08-27 19:16:09 +0800

    init platform

---
+++ client/golang/platform/platform.go
@@ -0,0 +1,54 @@
+//PlatForm abstract stb hw modules and give interfaces for logic module use
+//Basic hw init and os(kernel) should NOT be part of this module.
+//
+
+package platform
+
+import (
+	"os"
+	"errors"
+	"encoding/json"
+)
+
+//InitJSONErr shows init json file error
+var InitJSONErr = errors.New("Error in init json file")
+
+
+//HwModuler is the main module in stb
+type HwModuler interface {
+	//HwModInit should be purecoded and multi-called
+	//p *os.File should be stb describe json file
+	HwModInit(p *os.File) (n int, err error)
+}
+
+//HwModule is one hwmodule
+type HwModule struct {
+	name string
+	hwtype string
+	//is dynamic meas hwmodule will insert/eject in runtime(true)
+	isdynamic bool
+	//is singleinst means only one instance use hw module(true) in one time
+	issingleinst bool
+	//absolute path in kernel
+	hwpath string
+	//hw specify attr string list
+	hwattr string
+}
+//PlatForm give the  
+//
+type PlatForm struct {
+	stbid string 
+	stbversion int
+	modules []HwModule
+}
+
+//HwModInit returns 0 means success parse the file and init submodules.
+//HwModInit returns line num >0 and errors in which module is error.
+func (hwmod *HwModule)HwModInit(p *os.File) (n int, err error) {
+	var modules []HwModule
+	//info := p.Stat().FileInfo
+	//data := make([]byte, p.Stat().FileInfo.Size())
+	data := make([]byte, 255)
+	err = json.Unmarshal(data, &modules)
+	return 0, err
+}
